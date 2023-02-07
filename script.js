87b43c600b1b614d7c85d4ada16e47b168501a54
Daniel Hull <code@danielhull.com>
2023-02-07 13:03:09 +0000

    Manipulating the DOM to connect random quote to html elements

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-let apiQuotes = [];
+// Connecting the HTML to the JS
 
-// Get Quotes from API
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+// Get Quotes from API & defining the global variable of apiQuotes
+let apiQuotes = [];
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
   try {
@@ -8,15 +15,30 @@ async function getQuotes() {
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    // This is where you would catch your error.
+    // This is where you would catch your error with an alert() etc.
   }
 }
 
 // Show new quote
 function newQuote() {
   // Pick a random quote from API Quotes array
-  const randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(randomQuote);
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  //Check if Author field is null, if so replace with the string "unknown"
+  if (quote.author === null) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check quote length to determine styling to be applied
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  // Return the final quote text
+  quoteText.textContent = quote.text;
 }
 
 // On load
