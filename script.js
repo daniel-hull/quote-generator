fc68e9008d61a9e6b21d52ee6662ddaa75cb3b80
Daniel Hull <code@danielhull.com>
2023-02-07 09:16:12 +0000

    Fetch quotes array from URL and randomly log a single quote in the console

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let apiQuotes = [];
 
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // This is where you would catch your error.
+  }
+}
+
+// Show new quote
+function newQuote() {
+  // Pick a random quote from API Quotes array
+  const randomQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(randomQuote);
+}
+
+// On load
+getQuotes();
