932a4d180b21d7534b6fd3fe9c793a8044f41ef0
Daniel Hull <code@danielhull.com>
2023-02-07 13:27:22 +0000

    Adding the loader to the web page, until fetch request is completed

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show that we are loading the page
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading once completed
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // Get Quotes from API & defining the global variable of apiQuotes
 let apiQuotes = [];
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -21,6 +35,7 @@ async function getQuotes() {
 
 // Show new quote
 function newQuote() {
+  loading();
   // Pick a random quote from API Quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //Check if Author field is null, if so replace with the string "unknown"
@@ -36,6 +51,9 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
+
+  // Set Quote, Hide Loader
+  complete();
 
   // Return the final quote text
   quoteText.textContent = quote.text;
